4d0972037650890ca825ad1bfe09dab7e134eaf1
Sam Li <weisen.li@hotmail.com>
2025-10-27 01:08:08 +0000

    add default style

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
     './components/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
+    extend: {
+      colors: {
+        'brand-blue': '#00205B',
+        'brand-red': '#FF0000',
+        'brand-grey': '#444444',
+      },
+      fontFamily: {
+        primary: ['Montserrat', 'sans-serif'],
+        secondary: ['Lato', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
